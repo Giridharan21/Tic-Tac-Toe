c405ac1f52513a5ecb8e536775717f5866955f35
Giridharan <48674576+Giridharan21@users.noreply.github.com>
2019-12-16 19:45:45 +0530

    Add files via upload
    Fixed some logical errors and added a slight change to design

--- index.js
+++ index.js
@@ -1,36 +1,14 @@
-if(localStorage.getItem('row')===null){
-	location.href='enterPage.html';
-}
-else{
-	var row=localStorage.getItem('row');
-	if(row>3){
-		let table=document.getElementById('table'),tr =document.getElementsByTagName('tr'), len=tr.length ;
-		for(j=0;j<len;j++){
-			for(i=3;i<row;i++){
-				tr[j].innerHTML+='<td></td>';
-			}
-		}
-		for(i=3;i<row;i++){
-			table.innerHTML+=tr[0].innerHTML;
-		}
-		
-	}
-}
+//div containing the table for tic tac toe game
+var gameBox=document.getElementById('gameContent');
+//div containing the input field for denoting the grid size
+var rowBox=document.getElementById('numOfRowsContainer');
+var numOfRows,len;
+//game is hidden by default and it is shown when grid size in entered in input field
+gameBox.style.display='none';
+//adding event listener to restart button to reset the values of all cells
 document.getElementById('restart').addEventListener('click',restart);
-var tdElements = document.getElementsByTagName('td'),len=tdElements.length,x='X',h3=document.getElementById('result');
-var a=[],temp=0,numOfRows=Math.sqrt(len), start=numOfRows-1;
-for(let i=0;i<len;i++){
-	tdElements[i].addEventListener('click',input);
-}
-for(let i=0;i<len;i++){
-	if(i%numOfRows===0){
-		start-=(numOfRows-1);
-		temp=start;
-	}
-	a[i]=temp;
-	temp+=(numOfRows-1);
-}
-
+var a=[],x='X' ,h3=document.getElementById('result'),tdElements;
+//wheneven a cell is clicked this function is called
 function input(){
 	if(this.innerHTML===''){
 	this.innerHTML=x;
@@ -39,40 +17,99 @@ function input(){
 	checkTable();
 	}
 }
+//restart function
 function restart(){
 	h3.innerHTML="";
 	x='X';
 	for(let i=0;i<len;i++)
 		tdElements[i].innerHTML='';
 }
+//function to check current values of all cells each time when a cell is click to determine the winner
 function checkTable(){
 	let row=0,dia1=0,dia2=0,col=0,count=0;
+	//looping through every cell in table. Consider  grid size is 3 and if i=2 , a row and a column is checked for no. of X's at same time.
+	//Array a contains the value to check row and column in one for loop.
 	for(let i=0;i<len;i++){
-		if(i!==0&&i%numOfRows===0){
-			if(row===numOfRows) {h3.innerHTML="X wins!!! Restarting game ";;setTimeout(restart,3000);}
-			else if(col===numOfRows) {h3.innerHTML="X wins!!! Restarting game ";setTimeout(restart,3000);}
-			else if(row===-numOfRows) {h3.innerHTML="O wins!!! Restarting game ";setTimeout(restart,3000);}
-			else if(col===-numOfRows) {h3.innerHTML="O wins!!! Restarting game ";setTimeout(restart,3000);}
-			//console.log(row+"  "+ col);
+		if((i!==0&&i%(numOfRows)==0)){
+			if(row==numOfRows||col==numOfRows) {
+				h3.innerHTML="X wins!!! Restarting game ";
+				setTimeout(restart,3000);
+			}
+			else if(row===-numOfRows || col===-numOfRows) {
+				h3.innerHTML="O wins!!! Restarting game ";
+				setTimeout(restart,3000);
+			}
+			//every nth time row and col value is set to 0, n denotes the grid size.
 			row=0;col=0;
 		}
 		if(tdElements[i].innerHTML==='X') row++;
 		else if(tdElements[i].innerHTML==='O') row--;
 		if(tdElements[i+a[i]].innerHTML==='X') col++;
 		else if (tdElements[i+a[i]].innerHTML==='O') col--;
-		if(i%(numOfRows+1)===0&&(tdElements[i].innerHTML==='X')) dia1++;
-		else if(i%(numOfRows+1)===0&&(tdElements[i].innerHTML==='O')) dia1--;
-		if(i!==0&&i%(numOfRows-1)===0&&(tdElements[i].innerHTML==='X')) dia2++;
-		else if(i!==0&&i%(numOfRows-1)===0&&(tdElements[i].innerHTML==='O')) dia2--;
+		
+		
+		if(i%(numOfRows+1)==0&&(tdElements[i].innerHTML==='X')) dia1++;
+		else if(i%(numOfRows+1)==0&&(tdElements[i].innerHTML==='O')) dia1--;
+		if(i!==0&&i%(numOfRows-1)==0&&(tdElements[i].innerHTML==='X')&&i!==len-1) dia2++;
+		else if(i!==0&&i%(numOfRows-1)==0&&i!==len-1&&(tdElements[i].innerHTML==='O')) dia2--;
 		if(tdElements[i].innerHTML!=='') count++;
+		
 	}
-	if(dia1===numOfRows||dia2===numOfRows) {h3.innerHTML="X wins!!! Restarting game ";setTimeout(restart,3000);}
-	else if(dia1===-numOfRows||dia2===-numOfRows) {h3.innerHTML="O wins!!! Restarting game ";setTimeout(restart,3000);}
+	console.log(dia2);
+	//condition to determine the winner
+	if(row==numOfRows||col==numOfRows) {
+		h3.innerHTML="X wins!!! Restarting game ";
+		setTimeout(restart,3000);
+	}
+	else if(row===-numOfRows || col===-numOfRows) {
+		h3.innerHTML="O wins!!! Restarting game ";
+		setTimeout(restart,3000);
+	}
+	//condition to draw match
+	if(dia1==numOfRows||dia2==numOfRows) {
+		h3.innerHTML="X wins!!! Restarting game ";setTimeout(restart,3000);
+	}
+	else if(dia1===-numOfRows||dia2===-numOfRows) {
+		h3.innerHTML="O wins!!! Restarting game ";setTimeout(restart,3000);
+	}
 	if(count==len){
 		h3.innerHTML="Match draw !!! Restarting game "; setTimeout(restart,3000);
 	}
 }
-document.getElementById('close').addEventListener('click',windowClose);
-function windowClose(){
-	location.href='enterPage.html';
+function home(){
+	rowBox.style.display='block';
+	gameBox.style.display='none';
+}
+//Game function to display required table for tic tac toe
+function game(){
+	numOfRows=parseInt(document.getElementsByTagName('input')[0].value);
+	var temp=0,start=numOfRows-1;
+	if(numOfRows<3||numOfRows>10) alert('Can\'t be empty or less than 3 or greater than 10');
+	else{
+		let table=document.getElementById('table'),tr =document.getElementsByTagName('tr'), trlen=tr.length;
+		for(j=0;j<trlen;j++){
+			for(i=3;i<numOfRows;i++){
+				tr[j].innerHTML+='<td></td>';
+			}
+		}
+		for(i=3;i<numOfRows;i++){
+			table.innerHTML+=tr[0].innerHTML;
+		}
+		tdElements=document.getElementsByTagName('td');
+		len=tdElements.length;
+		for(let i=0;i<len;i++){
+			tdElements[i].addEventListener('click',input);
+		}
+		for(let i=0;i<len;i++){
+			if(i%numOfRows===0){
+				start-=(numOfRows-1);
+				temp=start;
+			}
+			a[i]=temp;
+			temp+=(numOfRows-1);
+		}
+		rowBox.style.display='none';
+		gameBox.style.display='block';
+		document.getElementById('home').addEventListener('click',home);
+	}
 }
